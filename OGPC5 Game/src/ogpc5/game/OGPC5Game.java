036faee734b3bbd4963deba77e6763d2e135b6ab
RomulusAaron <RomulusAaron@gmail.com>
2012-01-09 04:30:13 +0000

    First Commit of the Project

---
+++ OGPC5 Game/src/ogpc5/game/OGPC5Game.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ogpc5.game;
+
+/**
+ *
+ * @author RomulusAaron
+ */
+public class OGPC5Game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
