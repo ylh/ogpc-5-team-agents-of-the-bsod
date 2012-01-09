bd10c9ccdaea852521a06f721555c574b3589fbc
RomulusAaron@gmail.com <RomulusAaron@gmail.com@3cf2341f-9897-3c07-8415-afd504afe4b8>
2012-01-09 04:30:13 +0000

    First Commit of the Project

---
+++ trunk/OGPC5 Game/src/ogpc5/game/CityGame.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ogpc5.game;
+
+import Game.Game;
+import java.awt.Graphics;
+
+/**
+ *
+ * @author RomulusAaron
+ */
+public class CityGame extends Game{
+
+    @Override
+    public void InitializeAndLoad() {
+        
+    }
+
+    @Override
+    public void UnloadContent() {
+        
+    }
+
+    @Override
+    public void Update() {
+        
+    }
+
+    @Override
+    public void Draw(Graphics grphcs) {
+        
+    }
+    
+}
