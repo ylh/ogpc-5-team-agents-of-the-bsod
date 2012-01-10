b243dc1248df018b7aa2611d564e53e312c4a28d
RomulusAaron@gmail.com <RomulusAaron@gmail.com@3cf2341f-9897-3c07-8415-afd504afe4b8>
2012-01-10 04:38:43 +0000

    Added the Stage Object, which will be where the logic for each area in the game is held

---
+++ trunk/OGPC5 Game/src/GamePlay/Stage.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GamePlay;
+
+import Player.Player;
+import WorldObjects.WorldObject;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Nekel_Seyew
+ */
+public class Stage {
+    private Player player;
+    private ArrayList<WorldObject> worldObjects;
+}
