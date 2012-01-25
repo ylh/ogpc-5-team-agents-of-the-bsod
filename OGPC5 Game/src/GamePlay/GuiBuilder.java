236f2b6eee19fa0a0cc60b3bbaff51b3430b8ed1
RomulusAaron <RomulusAaron@gmail.com>
2012-01-25 23:21:30 +0000

    added the first part for the GUI building and are fixing the Player class

---
+++ OGPC5 Game/src/GamePlay/GuiBuilder.java
@@ -0,0 +1,81 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GamePlay;
+
+import Utilities.ImageCollection;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+
+/**
+ *
+ * @author Ksweeney12
+ */
+public class GuiBuilder extends Utilities.InputAdvance{
+
+    public GuiBuilder(){
+        
+    }
+    
+    public void Draw(ImageCollection batch){
+        
+    }
+    
+    @Override
+    public void keyTyped(KeyEvent ke) {
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+        
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+        
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent me) {
+        
+    }
+
+    @Override
+    public void mousePressed(MouseEvent me) {
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent me) {
+        
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent me) {
+        
+    }
+
+    @Override
+    public void mouseExited(MouseEvent me) {
+        
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent me) {
+        
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent me) {
+        
+    }
+
+    @Override
+    public void mouseWheelMoved(MouseWheelEvent mwe) {
+        
+    }
+    
+}
