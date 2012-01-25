f3458632ead8fc8e6ecdb224914153457d44b8a8
RomulusAaron@gmail.com <RomulusAaron@gmail.com@3cf2341f-9897-3c07-8415-afd504afe4b8>
2012-01-25 23:21:30 +0000

    added the first part for the GUI building and are fixing the Player class

--- trunk/OGPC5 Game/src/Player/Player.java
+++ trunk/OGPC5 Game/src/Player/Player.java
@@ -4,7 +4,6 @@
  */
 package Player;
 
-import Utilities.Animation;
 import Utilities.Vector2;
 import java.util.ArrayList;
 import WorldObjects.WorldObject;
@@ -14,30 +13,11 @@ import WorldObjects.WorldObject;
  * @author Nekel_Seyew
  */
 public class Player {
-    protected Vector2 position;
-    protected int direction;//1=left, 2= right, 3=up, 4= down
-    //Animations
-    protected Animation walkLeft;
-    protected Animation walkRight;
-    protected Animation walkUp;
-    protected Animation walkDown;
-    protected Animation stillLeft;
-    protected Animation stillRight;
-    protected Animation stillUp;
-    protected Animation stillDown;
     
     public Player(Vector2 position, int direction){
-        this.position=position;
-        this.direction=direction;
-        this.getAnimations();
     }
     
     public void update(ArrayList<WorldObject> worldObjects){
-        
-    }
-    
-    //TODO: create all of the animations
-    private void getAnimations(){
         
     }
 }
