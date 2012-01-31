834dd383f3001d010e1c931cc5184b0a9161288b
tsutton1997@gmail.com <tsutton1997@gmail.com@3cf2341f-9897-3c07-8415-afd504afe4b8>
2012-01-31 23:33:58 +0000

    Creates Store, Adds Bounding Box to Tower

--- trunk/OGPC5 Game/src/WorldObjects/towers/Tower.java
+++ trunk/OGPC5 Game/src/WorldObjects/towers/Tower.java
@@ -4,7 +4,6 @@
  */
 package WorldObjects.towers;
 
-import Utilities.Image2D;
 import Utilities.ImageCollection;
 import Utilities.Vector2;
 import WorldObjects.WorldObject;
@@ -16,7 +15,17 @@ import java.util.ArrayList;
  */
 public class Tower extends WorldObject {
     
-    protected double bonus;
+    protected int bonus;
+    protected double strength;  //Base damage dealt
+    protected double health;
+    
+    /*
+     * Coordinates for bounding box
+     */
+    protected int bx1;
+    protected int bx2;
+    protected int by1;
+    protected int by2;
 
     public Tower(Vector2 pos, int dir, String spritePath){
         super(pos, dir, spritePath);
@@ -32,4 +41,34 @@ public class Tower extends WorldObject {
         
     }
     
+    public int getBonus(){
+        return bonus;
+    }
+    
+    public void setBonus(int b){
+        bonus = b;
+    }
+    
+    public double getStrength(){
+        return strength;
+    }
+    
+    public void setStrength(double s){
+        strength = s;
+    }
+    
+    public double getHealth(){
+        return health;
+    }
+    
+    public void setHealth(double h){
+        health = h;
+    }
+    
+    protected void setBoundingBox(){
+        bx1 = (int) ((int)sprite.getPosition().getX()-(.5*sprite.getIconWidth()));
+        bx2 = (int) ((int)sprite.getPosition().getX()+(.5*sprite.getIconWidth()));
+        by1 = (int) ((int)sprite.getPosition().getY()+(.5*sprite.getIconHeight()));
+        by2 = (int) ((int)sprite.getPosition().getY()-(.5*sprite.getIconHeight()));
+    }
 }
