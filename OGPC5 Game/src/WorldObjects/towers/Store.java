2fb14cb62678a9301e511a5a74f10c198875c31c
tsutton1997 <tsutton1997@gmail.com>
2012-01-31 23:33:58 +0000

    Creates Store, Adds Bounding Box to Tower

---
+++ OGPC5 Game/src/WorldObjects/towers/Store.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WorldObjects.towers;
+
+import Utilities.Vector2;
+
+/**
+ *
+ * @author tsutton14
+ */
+public class Store extends Tower {
+    
+    public Store(Vector2 pos, int dir, String spritePath){
+        super(pos, dir, spritePath);
+    }
+    
+}
