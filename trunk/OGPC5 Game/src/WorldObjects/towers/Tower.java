d5554dd7c3195892f96539ccb3c0a33c8d482d26
pcowal@gmail.com <pcowal@gmail.com@3cf2341f-9897-3c07-8415-afd504afe4b8>
2012-02-02 23:49:42 +0000

    It can shoot now!!! It targets the nearest enemy.

--- trunk/OGPC5 Game/src/WorldObjects/towers/Tower.java
+++ trunk/OGPC5 Game/src/WorldObjects/towers/Tower.java
@@ -15,14 +15,14 @@ import java.util.ArrayList;
  * @author tsutton14
  */
 public class Tower extends WorldObject {
-    
+
     protected int bonus;
-    protected double strength;  //Base damage dealt
+    protected double damage;  //Base damage dealt
     protected double health;
     protected double range;
     protected double adamage;  //Damage dealt to armor
     protected double sdamage;  //Damage dealt to speed
-    
+    protected double speed;
     /*
      * Coordinates for bounding box
      */
@@ -31,67 +31,83 @@ public class Tower extends WorldObject {
     protected int by1;
     protected int by2;
 
-    public Tower(Vector2 pos, int dir, String spritePath){
+    public Tower(Vector2 pos, int dir, String spritePath) {
         super(pos, dir, spritePath);
-    }
-    
-    @Override
-    public void Update(ArrayList<WorldObject> wol) {        
-        
     }
 
     @Override
-    public void Draw(ImageCollection batch) {        
-        
+    public void Update(ArrayList<WorldObject> wol) {
+        shoot(wol);
+
     }
-    
-    public int getBonus(){
+
+    @Override
+    public void Draw(ImageCollection batch) {
+    }
+
+    public int getBonus() {
         return bonus;
     }
-    
-    public void setBonus(int b){
+
+    public void setBonus(int b) {
         bonus = b;
     }
-    
-    public double getStrength(){
-        return strength;
+
+    public double getDamage() {
+        return damage;
     }
-    
-    public void setStrength(double s){
-        strength = s;
+    public double getArmorDamage() {
+        return adamage;
     }
-    
-    public double getHealth(){
+    public double getSpeedDamage() {
+        return sdamage;
+    }
+
+    public void setDamage(double s) {
+        damage = s;
+    }
+    public void setArmorDamage(double s) {
+        adamage = s;
+    }
+    public void setSpeedDamage(double s) {
+        sdamage = s;
+    }
+
+    public double getHealth() {
         return health;
     }
-    
-    public void setHealth(double h){
+
+    public void setHealth(double h) {
         health = h;
     }
-    
-    protected void setBoundingBox(){
-        bx1 = (int) ((int)sprite.getPosition().getX()-(.5*sprite.getIconWidth()));
-        bx2 = (int) ((int)sprite.getPosition().getX()+(.5*sprite.getIconWidth()));
-        by1 = (int) ((int)sprite.getPosition().getY()+(.5*sprite.getIconHeight()));
-        by2 = (int) ((int)sprite.getPosition().getY()-(.5*sprite.getIconHeight()));
+
+    protected void setBoundingBox() {
+        bx1 = (int) ((int) sprite.getPosition().getX() - (.5 * sprite.getIconWidth()));
+        bx2 = (int) ((int) sprite.getPosition().getX() + (.5 * sprite.getIconWidth()));
+        by1 = (int) ((int) sprite.getPosition().getY() + (.5 * sprite.getIconHeight()));
+        by2 = (int) ((int) sprite.getPosition().getY() - (.5 * sprite.getIconHeight()));
     }
-    public void shoot(WorldObject[] wo){
-        double minDistance=range;
-        
-        Vector2 displacement=new Vector2(range,0);
+
+    public void shoot(ArrayList<WorldObject> wo) {
+        double minDistance = range;
+
+        Vector2 displacement = new Vector2(range, 0);
         double distance;
-        WorldObject target=new Enemy(0,0,0,new Vector2(),"asdf");
-        for(WorldObject w: wo){
-            if(w instanceof Enemy){
-                displacement=w.getPosition();
+        WorldObject target = null;
+        for (WorldObject w : wo) {
+            if (w instanceof Enemy) {
+                displacement = w.getPosition();
                 displacement.subtract(position);
-                distance=displacement.length();
-                
-                if (distance<minDistance){
-                    target=w;
-                    minDistance=distance;
+                distance = displacement.length();
+
+                if (distance < minDistance) {
+                    target = w;
+                    minDistance = distance;
                 }
             }
+        }
+        if (target != null) {
+            wo.add(new Bullet(position, damage, adamage, sdamage, speed, target.getPosition()));
         }
     }
 }
