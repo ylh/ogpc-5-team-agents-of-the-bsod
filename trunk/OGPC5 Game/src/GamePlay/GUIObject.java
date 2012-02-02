5dc4990cc1404d8f2026dad6e3f0cc036708db20
RomulusAaron@gmail.com <RomulusAaron@gmail.com@3cf2341f-9897-3c07-8415-afd504afe4b8>
2012-02-02 00:24:23 +0000

    Keep working on GUIObject. Need to make it toggle-able to go go

--- trunk/OGPC5 Game/src/GamePlay/GUIObject.java
+++ trunk/OGPC5 Game/src/GamePlay/GUIObject.java
@@ -20,7 +20,7 @@ public class GUIObject {
     Rect boundingBox;
     Image2D sprite;
     Vector2 pos;
-    boolean shouldMove;
+    boolean shouldMove=true;
     
     public void setMove(boolean should){
         shouldMove=should;
@@ -29,16 +29,18 @@ public class GUIObject {
     public GUIObject(Vector2 position, String path){
         pos=position;
         sprite= new Image2D(path);
+        sprite.setVector2(pos);
         boundingBox=CityGame.getRectangle(sprite);
     }
     
     public void Update(ArrayList<GUIObject> guiobjects){
+        boundingBox=CityGame.getRectangle(sprite);
         for(GUIObject g: guiobjects){
-            boundingBox=CityGame.getRectangle(sprite);
-            if(!boundingBox.intersects(CityGame.getRectangle(g.sprite))&&shouldMove){
+            if(!boundingBox.intersects(CityGame.getRectangle(g.sprite))&&g!=this&&shouldMove){
                 pos.dY(5);
             }
         }
+        //pos.dY(5);
     }
     
     public void Draw(ImageCollection batch){
