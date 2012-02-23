f787dd693005053feba598b533712e94f18e9674
pcowal <pcowal@gmail.com>
2012-02-23 23:45:03 +0000

    smooth glidey motion!!!

--- OGPC5 Game/src/GUIStuff/Button.java
+++ OGPC5 Game/src/GUIStuff/Button.java
@@ -4,10 +4,39 @@
  */
 package GUIStuff;
 
+import Utilities.Vector2;
+import WorldObjects.towers.Tower;
+
 /**
  *
  * @author pcowal15
  */
 public class Button {
-    
+    Vector2 pos;
+    Vector2 openpos;
+    Vector2 closepos;
+    Tower tower;
+    double movespeed; //you want a value less than 0.5
+    boolean open;
+    public Button(Vector2 op, Vector2 cp, Tower t, double ms){
+        openpos=op;
+        closepos=cp;
+        pos=cp;
+        movespeed=ms;
+        open=false;
+    }
+    public void update(){
+        //open or closed?
+        Vector2 targetpos;
+        double dy;
+        if (open){
+            targetpos=openpos.clone();
+        }
+        else{
+            targetpos=closepos.clone();
+        }
+        dy=targetpos.getY()-pos.getY();
+        dy*=movespeed;
+        pos.dY(dy);
+    }
 }
