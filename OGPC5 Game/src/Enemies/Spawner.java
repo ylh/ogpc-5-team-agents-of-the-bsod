70839578bb12a9f28d863b9d7460759cf731647c
RomulusAaron <RomulusAaron@gmail.com>
2012-03-12 23:59:47 +0000

    Moved road things to ROAD.Java. Spawner is almost done. Merged my fixed road things with Taylor's

--- OGPC5 Game/src/Enemies/Spawner.java
+++ OGPC5 Game/src/Enemies/Spawner.java
@@ -26,12 +26,67 @@ public class Spawner {
         pos= new Vector2[632/32];
         quota=10;
     }
+    public void addLocation(int i, Vector2 p){
+        pos[i]=p;
+    }
 
     public void update(){
         double time=System.currentTimeMillis();
         if(time- lastTime >= timeToWait){
-            
+            double prob= Math.random()*100;
+            int position=0;
+            //NOT EFFICIENT. GAH!!!!!!!
+            for(int i=0; i<=11; i++){
+                double d=quota*(EnemyProbabilityTable.getProbability(i)/100.0);
+                for(int j=0; j<(int)d; j++){
+                    try {
+                        while (pos[position] == null) {
+                            position++;
+                        }
+                        addEnemy(i, position);
+                        position++;
+                    } catch (Exception e) {
+                        position = 0;
+                        if(pos[position]!=null){
+                            addEnemy(i, position);
+                            position++;
+                        }
+                    }
+                }
+            }
+            quota+=2;
            lastTime=time;
+        }
+    }
+    //TODO: finish this
+    private void addEnemy(int enemy, int position){//add the given enemy at the given location.
+        switch(enemy){
+            case Enemy.ARSONIST:
+                break;
+            case Enemy.CRIMINAL:
+                break;
+            case Enemy.EARTHQUACKE:
+                break;
+            case Enemy.EDUCATION:
+                break;
+            case Enemy.FIRE:
+                break;
+            case Enemy.FLOOD:
+                break;
+            case Enemy.GANGS:
+                break;
+            case Enemy.GENERIC:
+                break;
+            case Enemy.GRAFITTI:
+                break;
+            case Enemy.SMOG:
+                break;
+            case Enemy.TRASH:
+                break;
+            case Enemy.WATER_POLUTION:
+                break;
+            default:
+                break;
         }
     }
 }
--- OGPC5 Game/src/WorldObjects/towers/Road.java
+++ OGPC5 Game/src/WorldObjects/towers/Road.java
@@ -22,20 +22,11 @@ import ogpc5.game.CityGame;
 public class Road extends Tile {
     
     protected Mouse mouse = new Mouse();
-    protected CityGame city = new CityGame();
-    
+    protected CityGame city = new CityGame();  
     private static final int ROAD_UP_RIGHT = 0;
     private static final int ROAD_UP_LEFT= 1;
     private static final int ROAD_DOWN_RIGHT = 2;
     private static final int ROAD_DOWN_LEFT = 3;
-    private static final int ROAD_T_RIGHT = 4;
-    private static final int ROAD_OMNISECTION = 5;
-    private static final int ROAD_T_LEFT = 6;
-    private static final int ROAD_T_DOWN = 7;
-    private static final int ROAD_T_UP = 8;
-    private static final int ROAD_HORIZONTAL = 9;
-    
-    private static int roadIndex;
     
     
     public Road(Vector2 pos, String spritePath){
@@ -56,91 +47,85 @@ public class Road extends Tile {
         batch.addToCollection(sprite, 20, dp);
     }
     
-        public boolean isTileRoad(int i, int j, Tile[][] t){  //Used by Road to determine the shape it should take
+    
+    public static boolean isTileRoad(Tile[][] t,int i, int j){  //Used by Road to determine the shape it should take
         if(t[i][j] instanceof Road){
             return true;
         }
         return false;
     }
     
-    public static int setRoadShape(int i, int j, Tile[][] tiles){
-        if(isRoadDown(tiles, i, j) && isRoadUp(tiles, i, j) && isRoadRight(tiles, i, j) && isRoadLeft(tiles,i,j)){
-            roadIndex = ROAD_OMNISECTION;
-        }
-        else if(isRoadDown(tiles, i, j) && isRoadUp(tiles, i, j) && isRoadRight(tiles, i, j) && !isRoadLeft(tiles,i,j)){
-            roadIndex = ROAD_T_RIGHT;
-        }
-        else if(isRoadDown(tiles, i, j) && isRoadUp(tiles, i, j) && !isRoadRight(tiles, i, j) && isRoadLeft(tiles,i,j)){
-            roadIndex = ROAD_T_LEFT;
-        }
-        else if(isRoadDown(tiles, i, j) && !isRoadUp(tiles, i, j) && isRoadRight(tiles, i, j) && isRoadLeft(tiles,i,j)){
-            roadIndex = ROAD_T_DOWN;
-        }
-        else if(isRoadDown(tiles, i, j) && !isRoadUp(tiles, i, j) && isRoadRight(tiles, i, j) && !isRoadLeft(tiles,i,j)){
+    public static int setRoadShape(Tile[][] t,int i, int j){
+        int roadIndex=0;
+        if(isRoadDown(t, i, j) && !isRoadUp(t, i, j) && !isRoadRight(t, i, j)){
             roadIndex = ROAD_UP_RIGHT;
         }
-        else if(isRoadDown(tiles, i, j) && !isRoadUp(tiles, i, j) && !isRoadRight(tiles, i, j) && isRoadLeft(tiles,i,j)){
+        else if(isRoadDown(t, i, j) && !isRoadUp(t, i, j) && !isRoadLeft(t, i, j)){
             roadIndex = ROAD_UP_LEFT;
         }
-        else if(!isRoadDown(tiles, i, j) && isRoadUp(tiles, i, j) && isRoadRight(tiles, i, j) && isRoadLeft(tiles,i,j)){
-            roadIndex = ROAD_T_UP;
-        }
-        else if(!isRoadDown(tiles, i, j) && isRoadUp(tiles, i, j) && isRoadRight(tiles, i, j) && !isRoadLeft(tiles,i,j)){
+        else if(isRoadUp(t, i, j) && !isRoadDown(t, i, j) && !isRoadRight(t, i, j)){
             roadIndex = ROAD_DOWN_RIGHT;
         }
-        else if(!isRoadDown(tiles, i, j) && isRoadUp(tiles, i, j) && !isRoadRight(tiles, i, j) && isRoadLeft(tiles,i,j)){
+        else if(isRoadUp(t, i, j) && !isRoadDown(t, i, j) && !isRoadLeft(t, i, j)){
             roadIndex = ROAD_DOWN_LEFT;
         }
-        else if(!isRoadDown(tiles, i, j) && !isRoadUp(tiles, i, j) && isRoadRight(tiles, i, j) && isRoadLeft(tiles,i,j)){
-            roadIndex = ROAD_HORIZONTAL;
-        }
-        else if(!isRoadDown(tiles, i, j) && !isRoadUp(tiles, i, j) && isRoadRight(tiles, i, j) && !isRoadLeft(tiles,i,j)){
-            roadIndex = ROAD_HORIZONTAL;
-        }
-        else if(!isRoadDown(tiles, i, j) && !isRoadUp(tiles, i, j) && !isRoadRight(tiles, i, j) && isRoadLeft(tiles,i,j)){
-            roadIndex = ROAD_HORIZONTAL;
-        }
         else{
-            roadIndex = 100;
+            roadIndex = 4;
         }
         
         return roadIndex;
     }
-    
-    private static boolean isRoadUp(Tile[][] tiles, int i, int j){  //Checks if road is above
-        if(j-1>0){
-            if(tiles[i][j-1] instanceof Road){
-                return true;
+
+    private static boolean isRoadUp(Tile[][] t, int i, int j) {  //Checks if road is above
+        try {
+            if (t[i][j - 1] instanceof Tile) {
+                if (t[i][j - 1] instanceof Road) {
+                    return true;
+                }
             }
+            return false;
+        } catch (Exception e) {
+            return false;
         }
-        return false;
     }
-    
-    private static boolean isRoadDown(Tile[][] tiles, int i, int j){  //Checks if road is below
-        if(j+1<tiles[0].length){
-            if(tiles[i][j+1] instanceof Road){
-                return true;
+
+    private static boolean isRoadDown(Tile[][] t, int i, int j) {  //Checks if road is above
+        try {
+            if (t[i][j + 1] instanceof Tile) {
+                if (t[i][j + 1] instanceof Road) {
+                    return true;
+                }
             }
+            return false;
+        } catch (Exception e) {
+            return false;
         }
-        return false;
     }
-    
-    private static boolean isRoadRight(Tile[][] tiles, int i, int j){  //Checks if road is right
-        if(i+1<tiles.length){
-            if(tiles[i+1][j] instanceof Road){
-                return true;
+
+    private static boolean isRoadRight(Tile[][] t, int i, int j) {  //Checks if road is right
+        try {
+            if (t[i - 1][j] instanceof Tile) {
+                if (t[i - 1][j] instanceof Road) {
+                    return true;
+                }
             }
+            return false;
+        } catch (Exception e) {
+            return false;
         }
-        return false;
     }
-    
-    private static boolean isRoadLeft(Tile[][] tiles, int i, int j){  //Checks if road is left
-        if(i-1>0){
-            if(tiles[i-1][j] instanceof Road){
-                return true;
+
+    private static boolean isRoadLeft(Tile[][] t, int i, int j) {  //Checks if road is left
+        try {
+            if (t[i + 1][j] instanceof Tile) {
+                if (t[i + 1][j] instanceof Road) {
+                    return true;
+                }
             }
+            return false;
+        } catch (Exception e) {
+            return false;
         }
-        return false;
     }
     
     public static String returnSprite(int i){  //Returns proper image according to orientation
@@ -149,18 +134,7 @@ public class Road extends Tile {
             case 1: return "Game Resources/Sprites/UpLeft.png";
             case 2: return "Game Resources/Sprites/DownRight.png";
             case 3: return "Game Resources/Sprites/DownLeft.png";
-            case 4: return "Game Resources/Sprites/TRight.png";
-            case 5: return "Game Resources/Sprites/OmniSection.png";
-            case 6: return "Game Resources/Sprites/TLeft.png";
-            case 7: return "Game Resources/Sprites/TDown.png";
-            case 8: return "Game Resources/Sprites/TUp.png";
-            case 9: return "Game Resources/Sprites/BasicHorizontal.png";
             default: return "Game Resources/Sprites/BasicRoad.png";                
         }
     }
-    
-
-
-    
-    
 }
