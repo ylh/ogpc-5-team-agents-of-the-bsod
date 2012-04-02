8be716a7a150ab4675e63859a9529a1bf71cb548
tsutton1997 <tsutton1997@gmail.com>
2012-04-02 22:26:21 +0000

    Finished prototype EnemyNavigation.  Should be able to access an ideal path from an ArrayList.  Will need an external method to use the path.

---
+++ OGPC5 Game/src/Enemies/EnemyNavigation.java
@@ -0,0 +1,123 @@
+package Enemies;
+
+import GUIStuff.Tile;
+import Utilities.Vector2;
+import WorldObjects.towers.Road;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author tsutton14
+ */
+public class EnemyNavigation extends Thread {
+    
+    Tile[][] tiles;
+    int goalX;
+    int goalY;
+    int spawnX;
+    int spawnY;
+    ArrayList route = new ArrayList();
+    
+    public EnemyNavigation(Tile[][] t, String navName, int i, int j, int k, int r){
+        super(navName);
+        tiles = t;
+        goalX = i;
+        goalY = j;
+        spawnX = k;
+        spawnY = r;
+    }
+    
+    @Override
+    public void run(){
+        route = findPath();        
+    }
+    
+    private double scoreRoad(int i, int j){
+        double score;
+        
+        score = 1/(1+Math.sqrt(Math.pow(goalX-i,2)+Math.pow(goalY-j, 2)));
+        
+        return score;
+    }
+        
+    
+    private Vector2 checkSurrounding(int i, int j){                
+        int highX = 0;
+        int highY = 0;
+        double highScore = 0;
+        
+    try {
+            if (tiles[i][j - 1] instanceof Tile) {
+                if (tiles[i][j - 1] instanceof Road) {
+                    if(scoreRoad(i,j-1)>=highScore){
+                        highScore = scoreRoad(i,j-1);
+                        highX = i;
+                        highY = j-1;
+                    }
+                }
+            }            
+        } catch (Exception e) {            
+        }
+        try {
+            if (tiles[i][j + 1] instanceof Tile) {
+                if (tiles[i][j + 1] instanceof Road) {
+                    if(scoreRoad(i,j+1)>=highScore){
+                        highScore = scoreRoad(i,j-1);
+                        highX = i;
+                        highY = j+1;
+                    }
+                }
+            }            
+        } catch (Exception e) {            
+        }  
+        try {
+            if (tiles[i+1][j] instanceof Tile) {
+                if (tiles[i+1][j] instanceof Road) {
+                    if(scoreRoad(i+1,j)>=highScore){
+                        highScore = scoreRoad(i,j-1);
+                        highX = i+1;
+                        highY = j;
+                    }
+                }
+            }            
+        } catch (Exception e) {            
+        }  
+        try {
+            if (tiles[i-1][j] instanceof Tile) {
+                if (tiles[i-1][j] instanceof Road) {
+                    if(scoreRoad(i-1,j)>=highScore){
+                        highScore = scoreRoad(i,j-1);
+                        highX = i-1;
+                        highY = j;
+                    }
+                }
+            }            
+        } catch (Exception e) {            
+        }
+        
+        return new Vector2(highX,highY);
+    }
+    
+    private ArrayList findPath(){
+        ArrayList path = new ArrayList();
+        
+        int currentX = spawnX;
+        int currentY = spawnY;
+        
+        while(currentX != goalX && currentY != goalY){
+            Vector2 v = new Vector2();
+            v = checkSurrounding(currentX, currentY);
+            path.add(v);
+            currentX = (int)v.getX();
+            currentY = (int)v.getY();
+        }
+        
+        return path;        
+    }
+    
+    public ArrayList getPath(){
+        return route;
+    }
+
+    
+}
