a551c28027b8e3cbf7fa21aa7d2db6573946b4c8
RomulusAaron <RomulusAaron@gmail.com>
2012-04-06 06:38:24 +0000

    Fixed the spawner, and prevented the bottom road tile from being deleted.

--- OGPC5 Game/src/Enemies/Spawner.java
+++ OGPC5 Game/src/Enemies/Spawner.java
@@ -12,94 +12,114 @@ import ogpc5.game.CityGame;
  * @author Nekel-Seyew
  */
 public class Spawner {
-    private Vector2[] pos;
-    private int quota;
+    private Vector2 pos;
     private long timeToWait;
     private double lastTime;
+    private boolean isSpawning;
     CityGame theGame;
     
-    public Spawner(long waitTime, CityGame theGame){
+    //spawning variables
+    private int quota;
+    private double[] thisRoundQuota;
+    private int location=0;
+    private double spawnLast;
+    
+    public Spawner(long waitTime, CityGame theGame, Vector2 loc){
         timeToWait=waitTime;
         this.theGame=theGame;
         lastTime=0;
-        pos= new Vector2[632/32];
+        pos= loc;
         quota=10;
+        isSpawning=false;
+        thisRoundQuota=new double[11];
         for(int i = 0; i<12; i++){            
             EnemyProbabilityTable.setProbability(i, 20);
         }
     }
-    public void addLocation(int i, Vector2 p){
-        pos[i]=p;
-    }
 
     public void update(){
         double time=System.currentTimeMillis();
-        if(time- lastTime >= timeToWait){            
-            int position=0;
-            for(int i=0; i<=11; i++){
-                double d=quota*(EnemyProbabilityTable.getProbability(i)/100.0);
-                for(int j=0; j<(int)d; j++){
-                    try {
-                        while (pos[position] == null) {
-                            position++;
-                        }
-                        addEnemy(i, pos[position]);
-                        position++;
-                    } catch (Exception e) {
-                        position = 0;
-                        if(pos[position]!=null){
-                            addEnemy(i, pos[position]);
-                            position++;
-                        }
-                    }
-                }
+        if(!isSpawning && time- lastTime >= timeToWait){
+            isSpawning=true;
+            for(int i=0; i<11; i++){
+                thisRoundQuota[i]=quota*(EnemyProbabilityTable.getProbability(i)/100.0);
             }
-            quota+=2;
-           lastTime=time;
         }
+        if(isSpawning && time-spawnLast >=500){
+            if(location>=thisRoundQuota.length){
+                location=0;
+                isSpawning=false;
+                lastTime=time;//End spawning until next round
+            }else if(thisRoundQuota[location]!=0){
+                addEnemy(location);
+                thisRoundQuota[location]-=1;
+                spawnLast=time;
+            }else if(thisRoundQuota[location]<=0){
+                location++;
+            }
+        }
+        
+        
+        
+//        double time=System.currentTimeMillis();
+//        double innerTimerA=System.currentTimeMillis();
+//        if(time- lastTime >= timeToWait){            
+//            for(int i=0; i<=11; i++){
+//                double innerTimerB = System.currentTimeMillis();;
+//                if (innerTimerB - innerTimerA >= 500) {
+//                    double d = quota * (EnemyProbabilityTable.getProbability(i) / 100.0);
+//                    for (int j = 0; j < (int) d; j++) {
+//                        addEnemy(i);
+//                    }
+//                    innerTimerA = innerTimerB;
+//                }
+//            }
+//            quota+=2;
+//           lastTime=time;
+//        }
     }
     
-    private void addEnemy(int enemy, Vector2 position){//add the given enemy at the given location.
+    private void addEnemy(int enemy){//add the given enemy at the given location.
         Enemy e = null;
         switch(enemy){
             case Enemy.ARSONIST:
-                e = new Enemy(Enemy.ARSONIST, 1, 10, 0, new Vector2(13*32+16,18*32+16), "Game Resources/Sprites/PlaceHolderEnemy.png", theGame.tiles);
+                e = new Enemy(Enemy.ARSONIST, 1, 10, 0, pos.clone(), "Game Resources/Sprites/PlaceHolderEnemy.png", theGame.tiles);
                 break;
             case Enemy.CRIMINAL:
-                e = new Enemy(Enemy.CRIMINAL, 1, 10, 0, new Vector2(13*32+16,18*32+16), "Game Resources/Sprites/PlaceHolderEnemy.png", theGame.tiles);
+                e = new Enemy(Enemy.CRIMINAL, 1, 10, 0, pos.clone(), "Game Resources/Sprites/PlaceHolderEnemy.png", theGame.tiles);
                 break;
             case Enemy.EARTHQUACKE:
-                e = new Enemy(Enemy.EARTHQUACKE, 1, 10, 0, new Vector2(13*32+16,18*32+16), "Game Resources/Sprites/PlaceHolderEnemy.png", theGame.tiles);
+                e = new Enemy(Enemy.EARTHQUACKE, 1, 10, 0, pos.clone(), "Game Resources/Sprites/PlaceHolderEnemy.png", theGame.tiles);
                 break;
             case Enemy.EDUCATION:
-                e = new Enemy(Enemy.EDUCATION, 1, 10, 0, new Vector2(13*32+16,18*32+16), "Game Resources/Sprites/PlaceHolderEnemy.png", theGame.tiles);
+                e = new Enemy(Enemy.EDUCATION, 1, 10, 0, pos.clone(), "Game Resources/Sprites/PlaceHolderEnemy.png", theGame.tiles);
                 break;
             case Enemy.FIRE:
-                e = new Enemy(Enemy.FIRE, 1, 10, 0, new Vector2(13*32+16,18*32+16), "Game Resources/Sprites/PlaceHolderEnemy.png", theGame.tiles);
+                e = new Enemy(Enemy.FIRE, 1, 10, 0, pos.clone(), "Game Resources/Sprites/PlaceHolderEnemy.png", theGame.tiles);
                 break;
             case Enemy.FLOOD:
-                e = new Enemy(Enemy.FLOOD, 1, 10, 0, new Vector2(13*32+16,18*32+16), "Game Resources/Sprites/PlaceHolderEnemy.png", theGame.tiles);
+                e = new Enemy(Enemy.FLOOD, 1, 10, 0, pos.clone(), "Game Resources/Sprites/PlaceHolderEnemy.png", theGame.tiles);
                 break;
             case Enemy.GANGS:
-                e = new Enemy(Enemy.GANGS, 1, 10, 0, new Vector2(13*32+16,18*32+16), "Game Resources/Sprites/PlaceHolderEnemy.png", theGame.tiles);
+                e = new Enemy(Enemy.GANGS, 1, 10, 0, pos.clone(), "Game Resources/Sprites/PlaceHolderEnemy.png", theGame.tiles);
                 break;
             case Enemy.GENERIC:
-                e = new Enemy(Enemy.GENERIC, 1, 10, 0, new Vector2(13*32+16,18*32+16), "Game Resources/Sprites/PlaceHolderEnemy.png", theGame.tiles);
+                e = new Enemy(Enemy.GENERIC, 1, 10, 0, pos.clone(), "Game Resources/Sprites/PlaceHolderEnemy.png", theGame.tiles);
                 break;
             case Enemy.GRAFITTI:
-                e = new Enemy(Enemy.GRAFITTI, 1, 10, 0, new Vector2(13*32+16,18*32+16), "Game Resources/Sprites/PlaceHolderEnemy.png", theGame.tiles);
+                e = new Enemy(Enemy.GRAFITTI, 1, 10, 0, pos.clone(), "Game Resources/Sprites/PlaceHolderEnemy.png", theGame.tiles);
                 break;
             case Enemy.SMOG:
-                e = new Enemy(Enemy.SMOG, 1, 10, 0, new Vector2(13*32+16,18*32+16), "Game Resources/Sprites/PlaceHolderEnemy", theGame.tiles);
+                e = new Enemy(Enemy.SMOG, 1, 10, 0, pos.clone(), "Game Resources/Sprites/PlaceHolderEnemy.png", theGame.tiles);
                 break;
             case Enemy.TRASH:
-                e = new Enemy(Enemy.TRASH, 1, 10, 0, new Vector2(13*32+16,18*32+16), "Game Resources/Sprites/PlaceHolderEnemy", theGame.tiles);
+                e = new Enemy(Enemy.TRASH, 1, 10, 0, pos.clone(), "Game Resources/Sprites/PlaceHolderEnemy.png", theGame.tiles);
                 break;
             case Enemy.WATER_POLUTION:
-                e = new Enemy(Enemy.WATER_POLUTION, 1, 10, 0, new Vector2(13*32+16,18*32+16), "Game Resources/Sprites/PlaceHolderEnemy", theGame.tiles);
+                e = new Enemy(Enemy.WATER_POLUTION, 1, 10, 0, pos.clone(), "Game Resources/Sprites/PlaceHolderEnemy.png", theGame.tiles);
                 break;
             default:
-                e = new Enemy(1, 10, 0, new Vector2(13*32+16,18*32+16), "Game Resources/Sprites/PlaceHolderEnemy", theGame.tiles);
+                e = new Enemy(1, 10, 0, pos.clone(), "Game Resources/Sprites/PlaceHolderEnemy.png", theGame.tiles);
                 break;
         }
         theGame.addToWorldObjects(e);
