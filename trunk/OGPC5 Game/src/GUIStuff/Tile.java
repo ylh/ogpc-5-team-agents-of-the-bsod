659e9997b32626d9ccbd567a399e116ab90f1412
samstrba6612@gmail.com <samstrba6612@gmail.com@3cf2341f-9897-3c07-8415-afd504afe4b8>
2012-04-10 22:36:44 +0000

    I did some documentation for the Button, Credit, and Tile classes.

--- trunk/OGPC5 Game/src/GUIStuff/Tile.java
+++ trunk/OGPC5 Game/src/GUIStuff/Tile.java
@@ -13,27 +13,44 @@ import java.awt.Color;
 import java.util.ArrayList;
 
 /**
- *
+ *A tile is the main unit of the towers.
  * @author Nekel-Seyew
  */
 public class Tile extends WorldObject{
     
     private boolean isSelected;
 
+    /**
+     *This is a default constructor.
+     * @param pos 
+     */
     public Tile(Vector2 pos){
         super(pos, 0, "");
     }
-    
+    /**
+     * This selects this tile.
+     */
     public void select(){
         isSelected=true;
     }
+    /**
+     * This checks if the tile is selected.
+     * @return 
+     */
     public boolean isSelected(){
         return isSelected;
     }
+    /**
+     * This de-selects the tile.
+     */
     public void unselect(){
         isSelected=false;
     }
     
+    /**
+     * This draws the tile. It also draws a highlighting rectangle.
+     * @param batch The batch is the image collection used by the game.
+     */
     @Override
     public void Draw(ImageCollection batch){
         if(isSelected){
@@ -43,6 +60,10 @@ public class Tile extends WorldObject{
         }
     }
 
+    /**
+     * This updates the tile.
+     * @param wol It's the array of world objects in the game.
+     */
     @Override
     public void Update(ArrayList<WorldObject> wol) {
         
