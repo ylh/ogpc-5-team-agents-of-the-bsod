97b01cd12874bac4ffd2c67539ab4544229246d8
samstrba6612 <samstrba6612@gmail.com>
2012-04-10 22:36:44 +0000

    I did some documentation for the Button, Credit, and Tile classes.

--- OGPC5 Game/src/GUIStuff/Button.java
+++ OGPC5 Game/src/GUIStuff/Button.java
@@ -12,7 +12,7 @@ import Utilities.Vector2;
 import WorldObjects.towers.Tower;
 import WorldObjects.towers.GenericTower;
 /**
- *
+ *A button is an image that has a code. This code is executed out when the button is clicked on.
  * @author pcowal15
  */
 public class Button {
@@ -25,7 +25,14 @@ public class Button {
     boolean open;
     Image2D sprite;
     int id;
-
+    /**
+     * This is the button.
+     * @param i The id for the button.
+     * @param sp The sprite for the button.
+     * @param op The opened position of the button.
+     * @param cp The closed position of the button.
+     * @param ms How fast the button goes.
+     */
     public Button(int i, String sp, Vector2 op, Vector2 cp, double ms) {
         id=i;
         openpos = op;
@@ -37,14 +44,24 @@ public class Button {
         button = new Rect(pos, 100, 32);
     }
 
-    
+    /**
+     * This tells the game that the button is opened.
+     * @param o 
+     */
     public void setOpen(boolean o){
         open=o;
     }
+    /**
+     * This returns the id.
+     * @return 
+     */
     public int id(){
         return id;
     }
-
+    /**
+     * This makes the button move down as if it is opening if it is closed.
+     * It makes the button move back up as if it is closing if it is open.
+     */
     public void glide() {
         //open or closed?
         Vector2 targetpos;
@@ -63,7 +80,11 @@ public class Button {
         //reset the bounding box
         button = new Rect(pos, 32, 32);
     }
-
+    /**
+     * This tells the game if the button is clicked on.
+     * @param mouse This is the arrow that when it is over the button when the mouse is clicked, it executes the code.
+     * @return This tells the game if the button is opened or closed.
+     */
     public boolean isPressed(Mouse mouse) {
         if (open && mouse.isPressed(Mouse.LEFT_BUTTON)) {
             Vector2 q = mouse.location().clone();
@@ -79,6 +100,10 @@ public class Button {
             return false;
         }
     }
+    /**
+     * This draws the new position of the sprite.
+     * @param batch 
+     */
     public void draw(ImageCollection batch){
         batch.Draw(sprite,pos,10000);
     }
--- OGPC5 Game/src/GUIStuff/Credit.java
+++ OGPC5 Game/src/GUIStuff/Credit.java
@@ -9,31 +9,45 @@ import Utilities.Vector2;
 import java.awt.Color;
 
 /**
- *
+ *A credit is a way of giving credit to an individual who created the game and their role in the creation.
  * @author RomulusAaron
  */
 public class Credit {
     String creditString;
     Vector2 position;
     int velocity=1;
-    
+    /**
+     * This is the credit.
+     * @param creditString This is the credit itself
+     * @param position This is the position of the credit.
+     */
     public Credit(String creditString, Vector2 position){
         this.creditString=creditString;
         this.position=position;
     }
-    
+    /**
+     * This moves the credit down.
+     */
     public void update(){
         position.dY(velocity);
     }
-    
+    /**
+     * This shows the credit.
+     * @param batch 
+     */
     public void draw(ImageCollection batch){
         batch.DrawString(position, creditString, Color.red, 10);
     }
-    
+    /**
+     * This stops the credit's movement.
+     */
     public void stop(){
         velocity=0;
     }
-    
+    /**
+     * This gets the Y-coordinate of the credit.
+     * @return 
+     */
     public double getY(){
         return position.getY();
     }
