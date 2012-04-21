a0405388d5405ce0f0faa32e513d9eb675d5dc10
tsutton1997@gmail.com <tsutton1997@gmail.com@3cf2341f-9897-3c07-8415-afd504afe4b8>
2012-04-21 21:58:58 +0000

    Added Bad Education animation

--- trunk/OGPC5 Game/src/ogpc5/game/CityGame.java
+++ trunk/OGPC5 Game/src/ogpc5/game/CityGame.java
@@ -137,7 +137,7 @@ public class CityGame extends Game {
 
         //For the main game
         UpdateAllStart = startTime;//Convienience, means nothing really....
-        spawn = new Spawner((1000) * 100, this, new Vector2(13 * 32 + 16, 18 * 32 + 16));
+        spawn = new Spawner((1000) * 5, this, new Vector2(13 * 32 + 16, 18 * 32 + 16));
         
         //navigator = new EnemyNavigation(tiles, "Default", 10, 10);
     }
