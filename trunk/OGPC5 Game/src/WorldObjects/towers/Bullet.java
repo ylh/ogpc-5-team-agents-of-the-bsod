576c7e46e3501451bbe7c816ab4f3196660031f2
tsutton1997@gmail.com <tsutton1997@gmail.com@3cf2341f-9897-3c07-8415-afd504afe4b8>
2012-04-25 22:35:22 +0000

    Added documentation to Bullet.

--- trunk/OGPC5 Game/src/WorldObjects/towers/Bullet.java
+++ trunk/OGPC5 Game/src/WorldObjects/towers/Bullet.java
@@ -51,6 +51,17 @@ public class Bullet extends WorldObject {
         this.sdamage=sdamage;
         sprite=new Image2D("Game Resources/Sprites/particle.png");
     }
+    
+    /**
+     * Alternate constructor for the bullet class
+     * @param s  the file path of the Image2D
+     * @param pos the starting position for the bullet
+     * @param damage the amount of damage the bullet will inflict
+     * @param adamage the amount of armor damage the bullet will inflict
+     * @param sdamage the amount of speed damage the bullet will inflict
+     * @param velocity the velocity the bullet travels at
+     * @param t the Target of the bullet
+     */
     public Bullet(String s, Vector2 pos, double damage,double adamage,double sdamage, double velocity, WorldObject t){
         super(pos.clone(), -1, "Hi there");
         loadAnimation();
@@ -67,7 +78,10 @@ public class Bullet extends WorldObject {
         sprite=new Image2D(s);
     }
     
-
+    /**
+     * Moves the bullet from startPos towards target
+     * @param wol the ArrayList of all WorldObjects
+     */
     @Override
     public void Update(ArrayList<WorldObject> wol) {
         double sx=startPos.getX();
@@ -88,6 +102,10 @@ public class Bullet extends WorldObject {
         return distance>1;
         //return false;
     }
+    
+    /**
+     * Determines if the bullet hits the target and determines damage to deal
+     */
     public void HitTarget(){
         AbstractEnemy e;
         Tower t;
@@ -104,7 +122,11 @@ public class Bullet extends WorldObject {
         
          
     }
-
+    
+    /**
+     * Draws the bullet
+     * @param batch ImageCollection to be drawn
+     */
     @Override
     public void Draw(ImageCollection batch) {
         if(spriteAnimation!=null){
@@ -115,16 +137,33 @@ public class Bullet extends WorldObject {
         }
     }
     
+    /**
+     * Sets the bullet's animation to a default animation of length 1
+     */
     public void loadAnimation(){
         spriteAnimation= new Animation(1,1, position, 1000);
     }
     
+    /**
+     * Get damage
+     * @return damage
+     */
     public double getDamage(){
         return damage;
     }
+    
+    /**
+     * Gets damage for armor
+     * @return adamage 
+     */
     public double getArmorDamage(){
         return adamage;
     }
+    
+    /**
+     *Gets damage due to speed 
+     * @return sdamage
+     */
     public double getSpeedDamage(){
         return sdamage;
     }
