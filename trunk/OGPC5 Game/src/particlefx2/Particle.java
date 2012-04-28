27f0ee9934ca25e99c6f9ef69e16701afb14b165
tsutton1997@gmail.com <tsutton1997@gmail.com@3cf2341f-9897-3c07-8415-afd504afe4b8>
2012-04-28 02:46:46 +0000

    Documented Particle.

--- trunk/OGPC5 Game/src/particlefx2/Particle.java
+++ trunk/OGPC5 Game/src/particlefx2/Particle.java
@@ -19,6 +19,13 @@ public class Particle {
     public Vector2 position;
     public double gravity;
 
+    /**
+     * Initializes a particle with motion and existence properties
+     * @param s the sprite of the particle
+     * @param p the position of the particle
+     * @param v the velocity of the particle
+     * @param g the gravity acting on the particle
+     */
     Particle(Image2D s, Vector2 p, Vector2 v, double g){
         velocity=v.clone();
         position=p.clone();
@@ -26,20 +33,35 @@ public class Particle {
         sprite=s;
     }
 
+    /**
+     * Gets the x value of the particle
+     * @return x value
+     */
     public double getX(){
         return position.getX();
     }
 
+    /**
+     * Gets the y value of the particle
+     * @return y value
+     */
     public double getY(){
         return position.getY();
     }
 
+    /**
+     * Moves the particle by accounting for velocity and gravity
+     */
     public void update(){
         velocity.dY(gravity);
         position.dX(velocity.getX());
         position.dY(10);
     }
 
+    /**
+     * Draws the particle
+     * @param batch ImageCollection to be drawn
+     */
     public void draw(ImageCollection batch){
         batch.Draw(sprite,position,100);
     }
