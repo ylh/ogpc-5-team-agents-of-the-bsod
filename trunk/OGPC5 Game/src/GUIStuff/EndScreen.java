2138a3fdae305632cddb059e7acbad7e947198ea
tsutton1997@gmail.com <tsutton1997@gmail.com@3cf2341f-9897-3c07-8415-afd504afe4b8>
2012-04-28 02:27:45 +0000

    Documented EndScreen.

--- trunk/OGPC5 Game/src/GUIStuff/EndScreen.java
+++ trunk/OGPC5 Game/src/GUIStuff/EndScreen.java
@@ -19,27 +19,52 @@ public class EndScreen {
     boolean restart;
     boolean hasStarted;
     
+    /**
+     * Initializes start/restart value to false
+     */
     public EndScreen(){
         restart=false;
         hasStarted=false;
     }
+    
+    /**
+     * Determines if restarting is necessary
+     * @param k KeyBoard in use
+     */
     public void Update(KeyBoard k){
         if(System.currentTimeMillis()-start >=10 * 1000 || k.isKeyDown(KeyEvent.VK_SPACE)){
             restart=true;
         }
     }
+    
+    /**
+     * Starts the EndScreen
+     */
     public void start(){
         start=System.currentTimeMillis();
         hasStarted=true;
     }
+    
+    /**
+     * Checks if the screen has started
+     * @return hasStarted
+     */
     public boolean hasStarted(){
         return hasStarted;
     }
     
+    /**
+     * Checks if it should restart
+     * @return restart
+     */
     public boolean restart(){
         return restart;
     }
     
+    /**
+     * Draws text explaining defeat;
+     * @param batch ImageCollection for drawing
+     */
     public void Draw(ImageCollection batch){
         batch.DrawString(new Vector2(100,100), "Your citizenry no longer trust you to run their city.", 
                 Color.white, 10, ImageCollection.FONT_DIALOG_INPUT, ImageCollection.FONT_BOLD_ITALIC, 18);
